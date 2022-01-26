5d75dd02dba2b3cfa65d244923001f47f16b55d5
feliposz <feliposz@gmail.com>
2022-01-26 04:51:51 -0300

    HelloWorld

---
+++ Console/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Console/HelloWorld/README.md
@@ -0,0 +1,3 @@
+# HelloWorld  
+
+[Create a .NET console application using Visual Studio Code](https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-6-0)
