3522586e2665e1da2e92d66c059f7297bb1c8c3f
feliposz <feliposz@gmail.com>
2022-01-27 02:18:52 -0300

    TryingNuGet

---
+++ Console/TryingNuGet/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+public class Account
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public DateTime DOB { get; set; }
+}
+
+public class Program 
+{
+
+    static void Main(string[] args)
+    {
+        Account account = new Account
+        {
+            Name = "John Doe",
+            Email = "john@nuget.com",
+            DOB = new DateTime(1981, 6, 13, 11, 15, 0, DateTimeKind.Utc),
+        };
+
+        string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+        Console.WriteLine(json);
+    }
+}
---
+++ Console/TryingNuGet/README.md
@@ -0,0 +1,3 @@
+# TryingNuGet  
+
+[Install and use a package using the dotnet CLI](https://docs.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-using-the-dotnet-cli)
